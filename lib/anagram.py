12e6904b18aafc8c445807865dbb6445c1412d6e
Joshua <joshua.gatune@student.moringaschool.com>
2023-08-29 15:02:15 +0300

    done

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,10 @@
 # your code goes here!
+
+class Anagram:
+    def __init__(self, word):
+        self.word =word
+
+    def match(self, words):
+        lower_self_word = self.word.lower()
+        return [word for word in words if sorted(word.lower()) == sorted(lower_self_word)and word.lower() != lower_self_word]
+
